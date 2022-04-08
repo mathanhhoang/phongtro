7677fe3ac97aaffda81997037eac3b810ed2ae10
mathanhhoang <mathanhhoang2506@gmail.com>
2022-04-08 14:32:50 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "react-bootstrap";
+import "./App.css";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Login from "./components/Login/Login"
+import Register from "./components/Register/Register";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header/>
+        <div className="page-container">
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/register" element={<Register/>}/>
+            {/* <Route path="/checkout" element={<Register/>}/> */}
+          </Routes>
+
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,55 @@
+import {Link} from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+        <Link to="/">TMA</Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Trang chủ
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Giới thiệu
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Thông tin phòng
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Liên hệ
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex">
+            <Link to="/post">Đăng tin</Link>
+            <Link to="/login">Đăng Nhập</Link>
+            <Link to="/register">Đăng Kí</Link>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
